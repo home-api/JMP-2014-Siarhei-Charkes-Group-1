556566a1244beb1aff24fed872f0d09fd32a4d24
dialog.gg@gmail.com <2429744abC>
2014-10-28 16:59:03 +0300

    Task02: implement some notes

--- module04/src/task02/BankAccount.java
+++ module04/src/task02/BankAccount.java
@@ -1,6 +1,11 @@
 package task02;
 
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class BankAccount {
+
+    private static ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
 
     private int amount = 0;
 
@@ -8,42 +13,47 @@ public class BankAccount {
         amount = initialAmount;
     }
 
-    public synchronized int getAmount(int processTime) throws InterruptedException {
+    public int getAmount(int processTime) throws InterruptedException {
+        readWriteLock.readLock().lock();
         Thread.sleep(processTime);
+        readWriteLock.readLock().unlock();
         return amount;
     }
 
-    public synchronized void changeAmount(int diff, int processTime) throws InterruptedException {
+    public void changeAmount(int diff, int processTime) throws InterruptedException {
+        readWriteLock.writeLock().lock();
         Thread.sleep(processTime);
         amount += diff;
+        readWriteLock.writeLock().unlock();
     }
 
     public static void main(String[] args) {
         BankAccount bankAccount = new BankAccount(0);
 
-        Thread banker1 = new Thread(new Banker(bankAccount, 500));
+        Thread banker1 = new Thread(new Banker(bankAccount, 300));
+        Thread banker3 = new Thread(new Banker(bankAccount, 500));
         Thread banker2 = new Thread(new Banker(bankAccount, 400));
-        Thread client1 = new Thread(new Client(bankAccount, 400));
+        Thread client1 = new Thread(new Client(bankAccount, 1000));
         Thread client2 = new Thread(new Client(bankAccount, 500));
+        Thread client3 = new Thread(new Client(bankAccount, 1000));
+        Thread client4 = new Thread(new Client(bankAccount, 1000));
+        Thread client5 = new Thread(new Client(bankAccount, 1000));
 
         // The real-life example begins here
         try {
-            // banker1 begins the changing of the bank account
             banker1.start();
-            // a bit later client1 comes and wants to know the balance
-            // But this client can not to get the balance because it's changed now
             client1.start();
+            client3.start();
+            client4.start();
             Thread.sleep(2000);
 
-            // Now client comes and want to know the balance
-            // but this operation could take a bit long time
-            client2.start();
-            // at the same moment the banker tries to change the balance
-            // but he can not because client is viewing the balance
+            banker3.start();
             banker2.start();
+            client2.start();
+            client5.start();
 
             /*
-            Expected result: 100 100
+            Expected result: 100 100 100 200 300
             If you delete synchronized, result will be 0 200 - it's incorrect result
              */
         } catch (InterruptedException e) {
